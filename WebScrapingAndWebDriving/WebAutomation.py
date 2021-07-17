0d4e6cde0ece5a71e77a5969ef95db184f01a18b
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-17 01:47:47 -0700

    Update

--- WebScrapingAndWebDriving/WebAutomation.py
+++ WebScrapingAndWebDriving/WebAutomation.py
@@ -4,14 +4,6 @@ from selenium.webdriver.common.keys import Keys
 chrome_driver_path = "/Users/LucasZ/Documents/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-# driver.get("https://en.wikipedia.org/wiki/Main_Page")
-
-# all_portals = driver.find_element_by_link_text("All portals")
-
-# search = driver.find_element_by_name("search")
-# search.send_keys("Python")
-# search.send_keys(Keys.ENTER)
-
 driver.get("https://secure-retreat-92358.herokuapp.com/")
 
 fname_input = driver.find_element_by_xpath("/html/body/form/input[1]")
@@ -23,6 +15,3 @@ email_input.send_keys("gibberish@gmail.com")
 
 send_button = driver.find_element_by_xpath("/html/body/form/button")
 send_button.click()
-
-
-# driver.quit()
